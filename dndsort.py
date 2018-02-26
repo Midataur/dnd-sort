ee6e31907aa4e5aaa8389a0be3af5d6385b3af0c
Midataur <maxpetschack2@gmail.com>
2018-02-26 16:28:48 +1100

    v1.1
    Adds a special sort for gold and adds some more comments.

--- dndsort.py
+++ dndsort.py
@@ -7,28 +7,55 @@ import re
 #Special sorts
 def weap_sort(weapons):
     for x in range(len(weapons)):
-        damage = re.search('\d*d\d+(\+\d+)?',weapons[x]) #regex for damage formula
+        damage = re.search('\d*d\d+(\+\d+)?',weapons[x])
         if damage != None:
-            rmdflag = 1 if damage.string[damage.start()] == 'd' else 0
-            damage = eval(damage.string[damage.start()+rmdflag:damage.end()].replace('d','*'))
-        weapons[x] = [damage,weapons[x]]
+            rmdflag = 1 if damage.string[damage.start()] == 'd' else 0 #Remove d if it's the first char
+            damage = eval(damage.string[damage.start()+rmdflag:damage.end()].replace('d','*')) #Turn damage formula into equation
+        weapons[x] = [damage,weapons[x]] #Tag weapon with it's max damage
     weapons = sorted(weapons)[::-1]
     for x in range(len(weapons)):
         weapons[x] = weapons[x][1]
     return weapons
+
+def gold_sort(gold):
+    global rates
+    for x in range(len(gold)):
+        value = re.findall('\d+[csepg]p?',gold[x])
+        if len(value) > 0:
+            #Got some coin strings, figure out how valuble they are
+            total = 0
+            coinregxs = ['\d+cp?','\d+cp?','\d+ep?','\d+gp?','\d+pp?']
+            for k in value:
+                for y in range(5):
+                    instring = re.search(coinregxs[y],k)
+                    if instring != None:
+                        total += int(re.sub('[csegpCSEGP]','',instring.string))*rates[y]
+            gold[x] = [total,gold[x]]
+        else:
+            gold[x] = [None,gold[x]]
+    gold = sorted(gold)[::-1]
+    print gold
+    for x in range(len(gold)):
+        gold[x] = gold[x][1]
+    return gold
 
 filename = 'demo.txt' #Replace this with your char file 
 
 #Categories
 weap = ['WEAPONS/DAMAGE:\n'] #[W]
 stat = ['STATS AND STATUSES:\n'] #[S]
-gold = ['GOLD/TRADE CREDIT:\n'] #[G]
+gold = ['GOLD/VALUBLE THINGS:\n'] #[G]
 equip = ['EQUIPMENT:\n'] #[E]
 alch = ['ALCHEMY:\n'] #[A]
 magic = ['MAGIC/SPELLS:\n'] #[M]
 junk = ['JUNK/MISC:\n'] #[J]
 
-categories = [['S',stat],['G',gold],['W',weap,weap_sort],['E',equip],['A',alch],['M',magic],['J',junk]]
+#Value of each currency denomenation in copper. Defaults to dnd5e.
+#Order is copper, silver, electrum, gold, platinum.
+
+rates = [1,10,50,100,1000]
+
+categories = [['S',stat],['G',gold,gold_sort],['W',weap,weap_sort],['E',equip],['A',alch],['M',magic],['J',junk]]
 charfile = open(filename).readlines()
 
 #Read file
